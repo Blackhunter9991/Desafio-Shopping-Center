9ab80200d53f68dd084ca69320c1e9054d8d1cc3
isaias <isaias.oliveira@e-unicred.com.br>
2023-05-07 22:17:17 -0300

    testando coisas

--- Desafio Etapa 1 e 2/Data.java
+++ Desafio Etapa 1 e 2/Data.java
@@ -3,57 +3,99 @@ public class Data {
     private int mes;
     private int ano;
 
-    public Data(int dia, int mes, int ano){
-      
-    boolean bissexto = (ano % 400 == 0)||(ano % 4 == 0 && ano % 100 != 0);
-
-    if(bissexto == true && mes == 2 && dia > 29 || dia <= 0 && mes > 12){ 
-   
-  
-      
-      }
-
-  
-
-
-   }
-
-    public int getDia(){
-        return dia;
-   }
-     public int getMes(){
-        return mes;
-   }
-     public int getAno(){
-        return ano;
-   }
-
-     public void setDia(int dia){
+    public Data(int dia, int mes, int ano) {
         this.dia = dia;
-   }
-     public void setMes(int mes){
         this.mes = mes;
-   }
-     public void setAno(int ano){
         this.ano = ano;
-   }
+        validarData();
+    }
 
-     public String toString(){
-        return getDia()+"/"+getMes()+"/"+getAno();
-   }
+    public int getDia() {
+        return dia;
+    }
 
-     public boolean verificaAnoBissexto(){
-        if((getAno() % 400 == 0)||(getAno() % 4 == 0 && getAno() % 100 != 0))
-      {
-         return true;
+    public int getMes() {
+        return mes;
+    }
 
-      }else
-      {
-         return false;
-      }
-    
-   }
-   
+    public int getAno() {
+        return ano;
+    }
+
+    public void validarData() {
+        int dia = getDia();
+        int mes = getMes();
+        int ano = getAno();
+        setTods();
+        if ((verificaAnoBissexto()) && mes == 2 && dia <= 29) {
+            System.out.println("E ano bissexto\t " + getDia() + "/" + getMes() + "/" + getAno());
+            return;
+        } else {
+            erroDeEntrada();
+            System.out.println("nao e ano bissexto\t ");
+            return;
+        }
+        if ((mesesMenores(mes) && dia <= 30)) {
+            setDia(dia);
+            setMes(mes);
+            setAno(ano);
+            System.out.println("E ano valido\t " + getDia() + "/" + getMes() + "/" + getAno());
+
+        } else if(!mesesMenores(mes)) {
+            setDia(dia);
+            setMes(mes);
+            setAno(ano);
+
+        } else {
+            erroDeEntrada();
+            System.out.println("Alguma coisa invalida\t ");
+        }
+    }
+
+    public void setTods() {
+        if(condicaoDia(dia) && condicaoMes(mes)) {}
+        else {
+            erroDeEntrada();
+        }
+    }
+
+    public boolean condicaoDia(int dia) {
+        return (dia > 0 && dia < 31);
+    }
+
+    public boolean condicaoMes(int mes) {
+        return (mes > 0 || mes < 13);
+    }
+
+    public void erroDeEntrada() {
+        setDia(1);
+        setMes(1);
+        setAno(2000);
+    }
+
+    public boolean mesesMenores(int mes) {
+        return (mes == 4 || mes == 6 || mes == 9 || mes == 11);
+    }
+
+    public void setDia(int dia) {
+       this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String toString() {
+        return getDia() + "/" + getMes() + "/" + getAno();
+    }
+
+    public boolean verificaAnoBissexto() {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
 }
 
     
--- Desafio Etapa 1 e 2/teste.java
+++ Desafio Etapa 1 e 2/teste.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
+
 public class teste {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner dInt = new Scanner(System.in);
-        Data d1 = new Data(12, 2, 2012);
-        d1.setDia(dInt.nextInt());
-        d1.setMes(dInt.nextInt());
-        d1.setAno(dInt.nextInt());
-        System.out.println(d1);
-        System.out.println(d1.toString());
-
-        
-
-
-
-
+        int loop;
+        int aux = 0;
+        Data d1 = new Data(32, 2, 2021);
+        System.out.println("Digite Tods as datas que tu quer:");
+        loop = dInt.nextInt();
+        while (aux < loop) {
+            System.out.print("Digite dia:\t");
+            d1.setDia(dInt.nextInt());
+            System.out.print("Digite mes:\t");
+            d1.setMes(dInt.nextInt());
+            System.out.print("Digite ano:\t");
+            d1.setAno(dInt.nextInt());
+            d1.validarData();
+            System.out.println(d1);
+            System.out.println(d1.toString());
+            aux++;
+        }
     }
-    
 }
